8339eeffb9b985b00c9913c49ef2f1edceedbabc
TheBalthazar19 <rishavshekhar19@gmail.com>
2026-05-06 16:11:24 +0530

    fix: change client event listener to use once and clean up login logging

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 const { Client, GatewayIntentBits, Events } = require('discord.js');
 require('dotenv').config();
 require('../server');
+
 const interactionHandler = require('./handlers/interactionHandler');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
 
-client.on('clientReady', () => {
+client.once('clientReady', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
 client.on(Events.InteractionCreate, interactionHandler);
-console.log("TOKEN EXISTS:", !!process.env.TOKEN);
 
 client.login(process.env.TOKEN)
-  .then(() => console.log("LOGIN STARTED"))
   .catch(err => console.error("LOGIN ERROR:", err));
