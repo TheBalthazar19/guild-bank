21dfec53108db1ae0f3ae0b02484b15609b38cb0
TheBalthazar19 <rishavshekhar19@gmail.com>
2026-05-06 14:53:53 +0530

    fix: streamline client login process and add token existence check

--- src/index.js
+++ src/index.js
@@ -12,5 +12,9 @@ client.on('clientReady', () => {
 });
 
 client.on(Events.InteractionCreate, interactionHandler);
+console.log("TOKEN EXISTS:", !!process.env.TOKEN);
 
+client.login(process.env.TOKEN)
+  .then(() => console.log("LOGIN STARTED"))
+  .catch(err => console.error("LOGIN ERROR:", err));
 client.login(process.env.TOKEN);
